11c8a67f55f45d047b1335fe2f6c7e01ebf1443e
arobialamar <arobial.camesi@gmail.com>
2020-09-01 09:35:40 +0700

    app/src/main/java/arobial/ta/bukukondangan/LaporanKondangan.java

--- app/src/main/java/arobial/ta/bukukondangan/LaporanKondangan.java
+++ app/src/main/java/arobial/ta/bukukondangan/LaporanKondangan.java
@@ -9,18 +9,19 @@ import android.view.MenuItem;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LaporanKondangan extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laporan_kondangan);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_toolbar, menu);
         return true;
     }
+    
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.logout) {
             FirebaseAuth.getInstance().signOut();
